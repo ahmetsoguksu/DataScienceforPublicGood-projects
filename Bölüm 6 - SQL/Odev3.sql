8a085eac03e942ecd7faac1efdc3df6f4eb5c036
Ahmet Soğuksu <dzcahmet@gmail.com>
2022-01-19 18:31:09 +0300

    Create Odev3.sql

---
+++ Bölüm 6 - SQL/Odev3.sql
@@ -0,0 +1,21 @@
+-- app.patika.dev SQL Modulü Ödev 3 --
+
+--Soru 1
+
+SELECT country FROM country
+WHERE country LIKE 'A%a';
+
+--Soru 2
+
+SELECT country FROM country
+WHERE country LIKE '%_____n';
+
+--Soru 3
+
+SELECT title FROM film
+WHERE title ILIKE '%t%t%t%t%';
+
+--Soru 4
+
+SELECT * FROM film
+WHERE title LIKE 'C%' AND (length > 90) AND (rental_rate = 2.99);

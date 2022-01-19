713902cd06c097f05bb8774b279e5f7b74c473ec
Ahmet Soğuksu <dzcahmet@gmail.com>
2022-01-19 18:03:58 +0300

    Created Odev2

--- Bölüm 6 - SQL/Odev1.sql
+++ Bölüm 6 - SQL/Odev1.sql
@@ -1,15 +1,25 @@
 -- app.patika.dev SQL modulü Ödev 1 --
+
 -- 1. Soru 
+
 SELECT title, description FROM film;
+
 -- 2. Soru
+
 SELECT * FROM film 
 WHERE length > 60 AND length < 75;
+
 -- 3. Soru
+
 SELECT * FROM film 
 WHERE rental_rate = 0.99 AND (replacement_cost = 12.99 OR replacement_cost =28.99);
+
 -- 4. Soru
+
 SELECT last_name FROM customer 
 WHERE first_name = 'Mary';
+
 -- 5. Soru
+
 SELECT * FROM film 
 WHERE length <= 50 AND (rental_rate <> 2.99 AND rental_rate <> 4.99);

15b458c1a6761a6ffc310c34add915f391b7a2d5
Ahmet Soğuksu <dzcahmet@gmail.com>
2022-01-20 14:57:21 +0300

    Create Odev7.sql

---
+++ Bölüm 6 - SQL/Odev7.sql
@@ -0,0 +1,20 @@
+--- app.pakika.dev SQL Modülü Ödev 7 ---
+
+-- Soru 1
+SELECT rating, COUNT(*) FROM film
+GROUP BY rating;
+
+--Soru 2
+SELECT replacement_cost, COUNT(*) FROM film
+GROUP BY replacement_cost
+HAVING COUNT(*)>50;
+
+--Soru 3
+SELECT store_id, COUNT(customer_id) FROM customer
+GROUP BY store_id;
+
+--Soru 4
+SELECT country_id, COUNT(*) FROM city
+GROUP BY country_id
+ORDER BY COUNT(*) DESC
+LIMIT 1;
